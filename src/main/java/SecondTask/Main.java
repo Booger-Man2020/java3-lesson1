aeb018fac3439f2c293ac6481d472e83d45c4cf5
Booger-Man2020 <93678809+Booger-Man2020@users.noreply.github.com>
2022-03-28 18:30:04 +0300

    Add files via upload
    firstTask

--- src/main/java/SecondTask/Main.java
+++ src/main/java/SecondTask/Main.java
@@ -1,20 +1,43 @@
-package SecondTask;
-
-import java.util.ArrayList;
-import java.util.Collections;
+package FirstTask;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] array = new Integer[10];
-        for (int j = 0; j < array.length; j++) {
-            array[j] = j + 1;
-        }
-        ArrayList<Integer> arrayList = new ArrayList();
-        Collections.addAll(arrayList,array);
 
-        System.out.println(arrayList.toString());
+        Integer[] integers = new Integer[10];
+        for (int j = 0; j < integers.length; j++) {
+            integers[j] = j + 1;
+        }
+
+        for (int j = 0; j < integers.length; j++) {
+            System.out.print(integers[j]);
+        }
+        System.out.println();
+
+
+        int index1 = 6, index2 = 2;
+        Integer current1 = null, current2 = null;
+        for (int j = 0; j < integers.length; j++) {
+            if (j == index1) {           //если объект или srting то  equals
+                current1 = integers[j];
+                integers[j] = null;
+            } else if (j == index2) {
+                current2 = integers[j];
+                integers[j] = current1;
+            }
+        }
+
+        for ( int j = integers.length; j >=0; j--) {
+            if (j == index1){
+                integers[j]= current2;
+            } else if (j == index2){
+                integers[j]= current1;
+            }
+        }
+
+        for (int i = 0; i < integers.length; i++) {
+            System.out.println(integers[i]);
+        }
+
 
     }
-
-
 }
